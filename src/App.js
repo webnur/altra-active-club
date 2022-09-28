7a99d0accf1aa52aa0c110295d38f4b39cf4e91c
webnur <mdnuruli291@gmai.com>
2022-09-29 05:52:18 +0600

    website name added and show the title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello Bangladesh</h1>
+      <Header></Header>
     </div>
   );
 }
